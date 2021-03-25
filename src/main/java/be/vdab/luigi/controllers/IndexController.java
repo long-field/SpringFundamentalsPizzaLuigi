736749964c17cfe62157dab8663accc646583d4f
DownbeatYapper <71273117+DownbeatYapper@users.noreply.github.com>
2021-03-25 23:05:19 +0100

    Luigi001

---
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package be.vdab.luigi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalTime;
+
+@Controller
+@RequestMapping("/")
+class IndexController {
+    @GetMapping
+    public ModelAndView index() {
+        var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
+        return new ModelAndView("index", "moment", morgenOfMiddag);
+    }
+}
