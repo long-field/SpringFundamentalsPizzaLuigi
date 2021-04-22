6b2f07948f9a7b1b8cfd525bb38bbdcffd3479bb
DownbeatYapper <71273117+DownbeatYapper@users.noreply.github.com>
2021-04-22 13:15:10 +0200

    Luigi001

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -5,22 +5,25 @@ import be.vdab.luigi.domain.Persoon;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 @RequestMapping("/")
 class IndexController {
+    private final AtomicInteger aantalKeerBekeken = new AtomicInteger();
     @GetMapping
     public ModelAndView index() {
+
         var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
         var modelAndView = new ModelAndView("index", "moment", morgenOfMiddag);
         modelAndView.addObject("zaakvoerder",
                 new Persoon("Luigi", "Peperone", 7, true, LocalDate.of(1966, 1, 31),
                         new Adres("Grote markt", "3", 9700, "Oudenaarde")));
+        modelAndView.addObject("aantalKeerBekeken", aantalKeerBekeken.incrementAndGet());
         return modelAndView;
     }
 }
--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -9,25 +9,38 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("pizzas")
 class PizzaController {
+
     private final Pizza[] pizzas = {
             new Pizza(1, "Prosciutto", BigDecimal.valueOf(4), true),
             new Pizza(2, "Margherita", BigDecimal.valueOf(5), false),
             new Pizza(3, "Calzone", BigDecimal.valueOf(4), false)};
-
+    private List<Pizza> pizzasMetPrijs(BigDecimal prijs) {
+        return Arrays.stream(pizzas)
+                .filter(pizza -> pizza.getPrijs().compareTo(prijs) == 0)
+                .collect(Collectors.toList());
+    }
     @GetMapping
     public ModelAndView pizzas() {
         return new ModelAndView("pizzas", "pizzas", pizzas);
     }
+
     @GetMapping("{id}")
     public ModelAndView pizza(@PathVariable long id) {
         var modelAndView = new ModelAndView("pizza");
         Arrays.stream(pizzas).filter(pizza -> pizza.getId() == id).findFirst()
                 .ifPresent(pizza -> modelAndView.addObject("pizza", pizza));
         return modelAndView;
+    }
+
+    @GetMapping("prijzen/{prijs}")
+    public ModelAndView pizzasMetEenPrijs(@PathVariable BigDecimal prijs) {
+        return new ModelAndView("prijzen", "pizzas", pizzasMetPrijs(prijs));
     }
 
 
